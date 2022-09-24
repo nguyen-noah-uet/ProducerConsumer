bab0f9b1772dad329a783bf4724cf8f7b1393dcc
Nguyen Khai Hoan <20020666@vnu.edu.vn>
2022-09-24 20:25:10 +0700

    Add project files.

---
+++ ProducerConsumer/ProducerConsumer.cs
@@ -0,0 +1,84 @@
+﻿namespace ProducerConsumer;
+
+public class ProducerConsumer
+{
+	private Queue<int> _buffer;
+	private Semaphore _noOfEmptySlots;
+	private Semaphore _noOfFilledSlots;
+	private Semaphore _mutex;
+	private DateTime _start;
+
+	public ProducerConsumer(int bufferSize)
+	{
+		_buffer = new Queue<int>(bufferSize);
+		_noOfEmptySlots = new Semaphore(initialCount: bufferSize, maximumCount: bufferSize);
+		_noOfFilledSlots = new Semaphore(initialCount: 0, maximumCount: bufferSize);
+		_mutex = new Semaphore(initialCount: 1, maximumCount: 1);
+		_start = DateTime.Now;
+	}
+	public void Produce()
+	{
+		do
+		{
+			// Wait until _noOfEmptySlots > 0 (_buffer is not full), then decrement _noOfEmptySlots by 1.
+			_noOfEmptySlots.WaitOne();
+			// Acquire lock.
+			_mutex.WaitOne();
+
+			// Insert data.
+			#region CriticalSection
+			int producedValue = Random.Shared.Next(0, 100);
+			_buffer.Enqueue(producedValue);
+			Console.Write("Added " + producedValue + ".\t");
+			ShowInfo();
+			Thread.Sleep(1000);
+			#endregion
+
+			// Release lock.
+			_mutex.Release();
+			// Increment _noOfFilledSlots by 1.
+			_noOfFilledSlots.Release();
+
+
+
+		} while ((DateTime.Now - _start).Seconds < 10);
+	}
+
+
+	public void Consume()
+	{
+		do
+		{
+			// Wait until _noOfFilledSlot > 0 (_buffer is not empty), then decrement _noOfFilledSlot by 1.
+			_noOfFilledSlots.WaitOne();
+			// Acquire lock.
+			_mutex.WaitOne();
+
+			// Remove data
+			#region CriticalSection
+			int consumedValue = _buffer.Dequeue();
+			Console.Write("Removed " + consumedValue + ".\t");
+			ShowInfo();
+			Thread.Sleep(1000);
+			#endregion
+			// Release lock.
+			_mutex.Release();
+			// Increment _noOfEmptySlots by 1.
+			_noOfEmptySlots.Release();
+
+
+
+		} while ((DateTime.Now - _start).Seconds < 10);
+	}
+	private void ShowInfo()
+	{
+		Console.Write("Buffer: ");
+		foreach (int i1 in _buffer)
+		{
+			Console.Write(i1 + " ");
+		}
+
+		Console.WriteLine();
+	}
+
+}
---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProducerConsumer;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var producerConsumer = new ProducerConsumer(20);
+		Thread producerThread = new Thread(producerConsumer.Produce);
+		Thread consumerThread = new Thread(producerConsumer.Consume);
+		producerThread.Start();
+		consumerThread.Start();
+	}
+}
